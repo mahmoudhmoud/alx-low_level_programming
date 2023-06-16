9bbd90342bad56b8f4c69e84d35ab5a2203a673c
mahmoudhmoud <jadmclarence@gamil.com>
2023-06-16 16:54:45 -0700

    ada

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,17 +13,20 @@
 
 int main(void)
 {
-	int n, digit;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	digit = n % 10;
+int n, last_ligit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+last_ligit = n % 10;
 
-	if (digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
-	else if (digit == 0)
-		printf("Last digit of %i is %i and is 0\n", n, digit);
-	else if (digit < 6 && digit != 0)
-		printf("Last digit of %i is %i and is less than 6 and not 0\n, n", digit);
-	return (0);
+if (last_ligit > 5)
+printf("Last digit of %i is %i and is greater than 5\n", n, last_ligit);
+
+else if (last_ligit == 0)
+printf("Last digit of %i is %i and is 0\n", n, last_ligit);
+
+else if (last_ligit < 6 && last_ligit !=  0)
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, last_ligit);
+
+return (0);
 }
