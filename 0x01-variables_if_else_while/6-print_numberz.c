0be6058ec0d1bb9d349481aaf04963bbc227f31f
mahmoudhmoud <jadmclarence@gamil.com>
2023-06-16 17:16:28 -0700

    ada34dd09mah5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Print number by using putchar function
+ *
+ * Return: 0 (Success)
+*/
+
+int main(void)
+{
+int i = 0;
+
+while (i < 10)
+{
+putchar('0' + i);
+i++;
+}
+putchar('\n');
+return (0);
+}
