f229a3ad6e8023a92d80e8521b6768fe3ad09949
mahmoudhmoud <jadmclarence@gamil.com>
2023-06-22 09:34:38 -0700

    adr

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
+
 /**
 * main - tests function that prints if integer is positive or negative
 * Return: 0
 */
+
 
 int main(void)
 {
@@ -14,3 +16,4 @@ int main(void)
 
 	return (0);
 }
+
