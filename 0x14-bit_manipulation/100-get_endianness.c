45306c67fe50828bfb1748c90e6afca4196b2c51
mahmoudhmoud <jadmclarence@gamil.com>
2023-08-03 08:47:35 -0700

    duapda5433ldakdad

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * get_endianness - returns the endianness off system
+ *
+ * Return: 0 if big endian, 1 if small
+ */
+
+
+int get_endianness(void)
+{
+
+	unsigned long int n = 1;
+
+	return (*(char *)&n);
+
+}
