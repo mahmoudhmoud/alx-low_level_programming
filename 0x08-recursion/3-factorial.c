62c07f5588991dea0c5d2c9ed1919b7b22170811
mahmoudhmoud <jadmclarence@gamil.com>
2023-08-30 16:48:24 -0700

    duapkdjad3k

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * factorial - finnds factorial
+ * @n:  int
+ * Return: int
+ */
+
+
+int factorial(int n)
+{
+
+if (n < 0)
+{
+	return (-1);
+}
+else if (n == 0)
+{
+	return (1);
+}
+
+return (n * factorial(n - 1));
+
+}
