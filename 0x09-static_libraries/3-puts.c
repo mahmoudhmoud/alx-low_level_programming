bbeded00e0645e0eabecf0fbf0194b27f058ce34
mahmoudhmoud <jadmclarence@gamil.com>
2023-09-01 15:05:39 -0700

    adlupdaksl

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - prints a sstring, followed  by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++;
+	}
+	_putchar('\n');
+
+}
+
