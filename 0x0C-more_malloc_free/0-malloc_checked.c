f1ae205eb640b6b39dc86043049d17cc0dfba410
mahmoudhmoud <jadmclarence@gamil.com>
2023-09-07 13:36:56 -0700

    adlak

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * *malloc_checked - allocates  the memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array  initialized or NULL
+ */
+
+
+void *malloc_checked(unsigned int b)
+{
+
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+
+}
