3cf4067029607c51387f455cf434eb676125b76a
mahmoudhmoud <jadmclarence@gamil.com>
2023-09-25 14:03:34 -0700

    dafawl

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+
+/**
+ * free_listint - frees  a list of ints
+ * @head: pointer to the first node
+ *
+ * Return: void
+ */
+
+
+void free_listint(listint_t *head)
+{
+
+	listint_t *node;
+
+	while (head)
+	{
+		node = head;
+		head = head->next;
+		free(node);
+	}
+
+}
