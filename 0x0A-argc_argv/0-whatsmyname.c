eeb46575eec70afa60ea6cc7a088af224583b542
mahmoudhmoud <jadmclarence@gamil.com>
2023-10-27 16:37:12 -0700

    adfa

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - kayktab smya dyal program matbo3 bstar jdid
+ * @argc: int
+ * @argv: list
+ * Return: zero
+ */
+
+
+int main(int argc, char const *argv[])
+
+{
+
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
