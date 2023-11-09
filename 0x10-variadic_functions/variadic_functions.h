7bf404047518e89adbf2b56c072d8af7dfdb9257
mahmoudhmoud <jadmclarence@gamil.com>
2023-11-09 15:04:17 -0800

    afade

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,31 @@
+#ifndef VARIADIC_H
+#define VARIADIC_H
+
+
+#include <stdarg.h>
+#include <stdio.h>
+
+
+/**
+ * struct token - is for token
+ * @token: kidayar
+ * @f: dala modmaja
+ */
+
+
+typedef struct token
+{
+
+	char *token;
+	void (*f)(char *, va_list);
+
+} token_t;
+
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+
+#endif
