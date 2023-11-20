2e8116b09bfbad8a6f7221036d06202f9731b35b
mahmoudhmoud <jadmclarence@gamil.com>
2023-11-20 10:11:56 -0800

    dawz

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+
+/**
+ * add_nodeint - zido falawal
+ * @head: node lawal
+ * @n: kima
+ * Return: jdid
+ */
+
+
+listint_t *add_nodeint(listint_t **head, const int n)
+
+{
+
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->next = NULL;
+	new_node->n = n;
+	if (*head)
+		new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+
+}
