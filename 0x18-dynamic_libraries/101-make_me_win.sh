f6ffe5f456d40ebeca14c112d80c2293f027bc1e
mahmoudhmoud <jadmclarence@gamil.com>
2024-01-15 16:32:14 -0800

    afafadfwwez

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-wget -P /tmp https://github.com/mahmoudhmoud/alx-low_level_programming/raw/master/0x18-dynamic_libraries/hadak.so
-export LD_PRELOAD=/tmp/hadak.so
+wget -P /tmp https://github.com/mahmoudhmoud/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
